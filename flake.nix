a3b9550c3d4a1ccbb8a0cec6004c2725d9920ae1
Arnaud Spiwack <arnaud.spiwack@tweag.io>
2025-12-18 14:44:28 +0900

    Setting up

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "Cardano block costs";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.duckdb
+            pkgs.julia-bin
+          ];
+        };
+      }
+    );
+}
