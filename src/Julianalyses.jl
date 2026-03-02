bc42fb55198dc4b2d5c9e40e6592eb42a7dfb8e0
Arnaud Spiwack <arnaud.spiwack@tweag.io>
2026-03-02 17:58:15 +0900

    Ratio bench/min fee

--- src/Julianalyses.jl
+++ src/Julianalyses.jl
@@ -24,15 +24,45 @@ using TOML
 
 config = TOML.parsefile("config.toml")
 src_blocks = config["sources"]["blocks"]
+src_bench = config["sources"]["benchmark"]
 src_txs = config["sources"]["transactions"]
 
 # Connect to the DuckDB database file.
 con = DuckDB.DB(config["database"]["file"])
 
 # Data frames
-blocks = DataFrame(DuckDB.query(con, "SELECT \"slot#\", \"block_size\" FROM '$src_blocks'"))
-txs = DataFrame(DuckDB.query(con, "SELECT count(*) as num, \"#script_wits\" FROM '$src_txs' GROUP BY \"#script_wits\""))
+# blocks = DataFrame(DuckDB.query(con, "SELECT \"slot#\", \"block_size\" FROM '$src_blocks'"))
+# txs = DataFrame(DuckDB.query(con, "SELECT count(*) as num, \"#script_wits\" FROM '$src_txs' GROUP BY \"#script_wits\""))
+blocks = DataFrame(DuckDB.query(con,
+    """
+    SELECT
+      Block.block_no, Bench.mut_headerApply, SUM(Tx.min_fee) AS total_min_fee
+    FROM
+      '$src_blocks' as Block
+    JOIN read_csv('$src_bench', names=['slot', 'slotGap', 'totalTime', 'mut', 'gc', 'majGcCount', 'minGcCount', 'allocatedBytes', 'mut_forecast', 'mut_headerTick', 'mut_headerApply', 'mut_ErrorApplyingHeader', 'mut_blockTick', 'mut_blockApply', 'mut_ErrorApplyingBlock', 'blockBytes', 'extra_one', 'extra_two']) as Bench
+    ON
+      Block.slot_no = Bench.slot
+    JOIN '$src_txs' as Tx
+    ON
+      Block.block_no = Tx.block_no
+    WHERE
+      Bench.mut_ErrorApplyingHeader = false
+    GROUP BY
+      Block.block_no, Bench.mut_headerApply
+    ;"""))
+# unclear: except for the last column name which is a bit shifty and spans
+# several columns, why doesn't duckdb manage to parse this header?
 
+## Histogram of divergence between fee calculation and actual benchmark
+divergence = blocks.mut_headerApply ./ blocks.total_min_fee
+divergence_hist = histogram(divergence, bins=50, xlabel="Apply time / min fee calculation", ylabel="Frequency", title="Distribution of divergence ratio",  xscale=:log10, yscale=:log10)
+## I don't know why the reference line doesn't appear below, but at any rate, it
+## doesn't look very informative. I guess, with hindsight, that the fact that the
+## two axes aren't in the same unit means that the reference ought to be
+## something else.
+# scatter(blocks.total_min_fee, blocks.mut_headerApply, xlabel="Fee calculation", ylabel="Benchmark",  xscale=:log10, yscale=:log10)
+# plot!(blocks.total_min_fee, blocks.total_min_fee, label="Y = X")  # reference line
+# divergence_scatter= current()
 
 ## Histogram: distribution of block sizes
 # hist = barhist(
@@ -63,16 +93,16 @@ txs = DataFrame(DuckDB.query(con, "SELECT count(*) as num, \"#script_wits\" FROM
 # png(overtime,"overtime.png")
 
 ## Histogram: distribution of number of script witnesses
-script_wits_dist = barhist(
-    txs."#script_wits",
-    weights = txs.num,
-    bins = 10,
-    xlabel = "Number of script witnesses",
-    ylabel = "Count (logarithmic)",
-    yscale = :log10,
-    label = "mainnet",
-    title = "Histogram of number script witnesses (10 bins)",
-)
-png(script_wits_dist,"script-wits-distrib.png")
+# script_wits_dist = barhist(
+#     txs."#script_wits",
+#     weights = txs.num,
+#     bins = 10,
+#     xlabel = "Number of script witnesses",
+#     ylabel = "Count (logarithmic)",
+#     yscale = :log10,
+#     label = "mainnet",
+#     title = "Histogram of number script witnesses (10 bins)",
+# )
+# png(script_wits_dist,"script-wits-distrib.png")
 
 end # module Julianalyses
